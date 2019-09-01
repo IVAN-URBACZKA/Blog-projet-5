ffe1ab3844f7196692b2dfbb740abf91332cb48f
URBACZKA IVAN <46192105+IVAN-URBACZKA@users.noreply.github.com>
2019-09-01 12:57:16 +0200

    add router poo
    add router poo

---
+++ controller/Router.php
@@ -0,0 +1,59 @@
+<?php
+
+require('controller/frontend.php');
+require('controller/backend.php');
+
+class Router {
+
+
+public function routerRequest()
+{
+
+try {
+    if (isset($_GET['action'])) {
+        if ($_GET['action'] == 'listArticles') {
+            listArticlesHome();
+        }
+
+        elseif ($_GET['action'] == 'blog') {
+            listArticlesBlog();
+        }
+
+        elseif($_GET['action'] == 'adduser'){
+            addUser();
+        }
+
+        elseif($_GET['action'] == 'register'){
+            register();
+        }
+
+
+        elseif ($_GET['action'] == 'article') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                article();
+            }
+
+            else {
+                throw new Exception('Aucun identifiant de billet envoyé');
+            }
+}
+
+}
+
+else {
+    listArticlesHome();
+}
+
+}
+
+
+
+catch(Exception $e) {
+    echo 'Erreur : ' . $e->getMessage();
+}
+
+}
+
+
+}
+     
